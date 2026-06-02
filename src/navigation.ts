14516c791b1968e7edf729319b0f12bb2481e361
reyhubcom <94319232+reyhubcom@users.noreply.github.com>
2026-06-02 08:03:14 -0400

    Update navigation.ts

--- src/navigation.ts
+++ src/navigation.ts
@@ -115,7 +115,7 @@ export const headerData = {
       href: '#',
     },
   ],
-  actions: [{ text: 'Download', href: 'https://github.com/', target: '_blank' }],
+  actions: [{ text: 'EXCHANGE', href: '/exchange', target: '' }],
 };
 
 export const footerData = {
